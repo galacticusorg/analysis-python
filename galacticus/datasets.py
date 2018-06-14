62f211af839c76a3b8d9bdf2fd65c9ab1ae861d1
Alex Merson <alex.i.merson@gmail.com>
2018-06-13 20:17:49 -0700

    Added datasets.py and constants.py.

---
+++ galacticus/datasets.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+class DatasetClass(object):
+
+    def __init__(self,name=None,data=None,redshift=None,output=None):
+        self.name = name
+        self.data = data
+        self.redshift = redshift
+        self.output = output
+        return
+
+    
+    
