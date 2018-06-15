b5dfc986874cbe8db702707585aeadd492bab5b3
Alex Merson <alex.i.merson@gmail.com>
2018-06-14 21:28:59 -0700

    Added Simulations subdirectory and tutorial for how to read simulation specifications.

--- galacticus/__init__.py
+++ galacticus/__init__.py
@@ -9,6 +9,8 @@ DATASETS_PATH = None
 datasetsKeyName = "GALACTICUS_DATASETS"
 if datasetsKeyName in os.environ.keys():
     DATASETS_PATH = os.environ[datasetsKeyName]
+    if not DATASETS_PATH.endswith("/"):
+        DATASETS_PATH = DATASETS_PATH + "/"
 else:
     warningString = "WARNING! No path specified for Galacticus datasets.\n"+\
         "Specify the path in your environment variables using the variable name '"+\
@@ -20,9 +22,26 @@ GALACTICUS_PATH = None
 datasetsKeyName = "GALACTICUS_ROOT"
 if datasetsKeyName in os.environ.keys():
     GALACTICUS_PATH = os.environ[datasetsKeyName]
+    if not GALACTICUS_PATH.endswith("/"):
+        GALACTICUS_PATH = GALACTICUS_PATH + "/"
 else:
     warningString = "WARNING! No path specified for Galacticus source code.\n"+\
         "Specify the path in your environment variables using the variable name '"+\
         datasetsKeyName+"'."
     warnings.warn(warningString)
 
+
+def checkDatasetsPath():
+    if DATASETS_PATH is None:
+        msg = "ERROR! Unable to locate Galacticus datasets. "+\
+            "DATASETS_PATH = "+str(DATASETS_PATH)
+        raise RuntimeError(msg)
+    if not os.path.exists(DATASETS_PATH):
+        msg = "ERROR! Datasets path '"+DATASETS_PATH+"' does not exist."
+        raise RuntimeError(msg)
+    if not os.path.exists(DATASETS_PATH+"static"):
+        msg = "ERROR! Static datasets path '"+DATASETS_PATH+"static' does not exist."
+        raise RuntimeError(msg)
+    if not os.path.exists(DATASETS_PATH+"dynamic"):
+        os.makedirs(DATASETS_PATH+"dynamic")
+    return
