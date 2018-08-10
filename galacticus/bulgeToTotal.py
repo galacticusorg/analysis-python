c26e3d233fbdd4fdaa9bb0322aec8fed619fbe1b
Alex Merson <alex.i.merson@gmail.com>
2018-08-10 03:51:04 -0700

    Added unit test for BulgeToTotal class.

--- galacticus/bulgeToTotal.py
+++ galacticus/bulgeToTotal.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import sys
+import sys,os
 import numpy as np
 import unittest
 from .datasets import Dataset
@@ -74,3 +74,73 @@ class BulgeToTotal(Property):
         del GALS
         return DATA
 
+class UnitTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        from .galaxies import Galaxies
+        from .io import GalacticusHDF5
+        from .data import GalacticusData
+        from shutil import copyfile
+        # Locate the dynamic version of the galacticus.snapshotExample.hdf5 file.
+        DATA = GalacticusData()
+        self.snapshotFile = DATA.searchDynamic("galacticus.snapshotExample.hdf5")
+        self.removeExample = False
+        # If the file does not exist, create a copy from the static version.
+        if self.snapshotFile is None:
+            self.snapshotFile = DATA.dynamic+"/examples/galacticus.snapshotExample.hdf5"
+            self.removeExample = True
+            if not os.path.exists(DATA.dynamic+"/examples"):
+                os.makedirs(DATA.dynamic+"/examples")
+            copyfile(DATA.static+"/examples/galacticus.snapshotExample.hdf5",self.snapshotFile)
+        # Initialize the Totals class.
+        GH5 = GalacticusHDF5(self.snapshotFile,'r')
+        GALS = Galaxies(GH5Obj=GH5)
+        self.BULGE = BulgeToTotal(GALS)
+        return
+
+    @classmethod
+    def tearDownClass(self):
+        # Clear memory and close/delete files as necessary.
+        self.BULGE.galaxies.GH5Obj.close()
+        del self.BULGE
+        if self.removeExample:
+            os.remove(self.snapshotFile)
+        return
+
+    def testMatches(self):
+        funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name
+        print("UNIT TEST: BulgeToTotal: "+funcname)
+        print("Testing BulgeToTotal.matches() function")
+        redshift = 1.0
+        self.assertTrue(self.BULGE.matches("bulgeToTotalMassStellar",redshift))
+        self.assertFalse(self.BULGE.matches("diskMassStellar",redshift))
+        self.assertFalse(self.BULGE.matches("spheroidMassStellar",redshift))
+        print("TEST COMPLETE")
+        print("\n")
+        return
+
+    def testGet(self):
+        funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name
+        print("UNIT TEST: BulgeToTotal: "+funcname)
+        print("Testing BulgeToTotal.get() function")
+        redshift = 1.0
+        self.assertRaises(RuntimeError,self.BULGE.get,"aMissingProperty",redshift)
+        for name in ["MassStellar","StarFormationRate"]:
+            OUT = self.BULGE.galaxies.GH5Obj.selectOutput(redshift)
+            bulge = np.array(OUT["nodeData/spheroid"+name]) 
+            disk = np.array(OUT["nodeData/disk"+name])
+            data = bulge/(bulge+disk)
+            DATA = self.BULGE.get("bulgeToTotal"+name,redshift)
+            self.assertIsNotNone(DATA)
+            self.assertEqual(DATA.name,"bulgeToTotal"+name)
+            self.assertIsNotNone(DATA.data)
+            diff = np.fabs(data-DATA.data)
+            for i in range(len(diff)):
+                if np.isnan(diff[i]):
+                    self.assertEqual(disk[i]+bulge[i],0.0)
+                else:
+                    self.assertLessEqual(diff[i],1.0e-6)
+        print("TEST COMPLETE")
+        print("\n")
+        return
