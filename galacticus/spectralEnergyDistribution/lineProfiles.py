c968c5e706514990387ce53bc8bb0357712d999e
Andrew Benson <abenson@obs.carnegiescience.edu>
2021-01-25 18:32:23 -0800

    * Integrate emission line luminosity across the wavelength bin when   constructing SEDs.

--- galacticus/spectralEnergyDistribution/lineProfiles.py
+++ galacticus/spectralEnergyDistribution/lineProfiles.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python                                                                                                                                                          
 import sys
 import numpy as np
+from scipy.special import erf
 from ..constants import Pi
 
 class LineProfiles(object):
@@ -13,9 +14,10 @@ class LineProfiles(object):
         sigma =  np.stack([sigma]*len(wavelengths),axis=1).reshape(len(sigma),-1)
         # Compute amplitude for Gaussian
         amplitude = np.stack([lineLuminosity]*len(wavelengths),axis=1).reshape(len(lineLuminosity),-1)
-        amplitude /= (sigma*np.sqrt(2.0*Pi))
         # Compute luminosity
+        wavelengthsDelta = np.gradient(wavelengths)
         wavelengthsGrid = np.concatenate([wavelengths]*len(lineLuminosity)).reshape(-1,len(wavelengths))
+        wavelengthsDeltaGrid = np.concatenate([wavelengthsDelta]*len(lineLuminosity)).reshape(-1,len(wavelengthsDelta))
         lineWavelengths = np.stack([lineWavelength]*len(wavelengths),axis=1).reshape(len(lineLuminosity),-1)
-        luminosity = amplitude*np.exp(-((wavelengthsGrid-lineWavelengths)**2)/(2.0*(sigma**2)))
+        luminosity = amplitude*0.5*(erf((wavelengthsGrid+0.5*wavelengthsDeltaGrid-lineWavelengths)/np.sqrt(2.0)/sigma)-erf((wavelengthsGrid-0.5*wavelengthsDeltaGrid-lineWavelengths)/np.sqrt(2.0)/sigma))/wavelengthsDeltaGrid
         return luminosity
